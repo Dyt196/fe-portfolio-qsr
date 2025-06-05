7d71dbfcac327faa9fa07e41faf6098d5a95ef9a
Dyt196 <m.ikhmalhidayat@gmail.com>
2025-06-06 04:41:08 +0800

    Update Route

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -12,13 +12,32 @@
 
     <q-drawer v-model="leftDrawerOpen" show-if-above bordered class="bg-primary">
       <q-list>
-        <q-item class="menu-list active">
+        <q-item class="menu-font">
+          <q-item-section>My Portfolio</q-item-section>
+          <q-item-section side>X</q-item-section>
+        </q-item>
+        <q-item
+          clickable
+          ripple
+          :class="`menu-list ${checkPath('/') ? 'active' : ''}`"
+          @click="goPage('/')"
+        >
           <q-item-section> Home </q-item-section>
         </q-item>
-        <q-item class="menu-list">
+        <q-item
+          clickable
+          ripple
+          :class="`menu-list ${checkPath('/skill') ? 'active' : ''}`"
+          @click="goPage('/skill')"
+        >
           <q-item-section> Skills </q-item-section>
         </q-item>
-        <q-item class="menu-list">
+        <q-item
+          clickable
+          ripple
+          :class="`menu-list ${checkPath('/project') ? 'active' : ''}`"
+          @click="goPage('/project')"
+        >
           <q-item-section> Projects / Work </q-item-section>
         </q-item>
       </q-list>
@@ -32,10 +51,25 @@
 
 <script setup lang="ts">
 import { ref } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
 
 const leftDrawerOpen = ref(false);
+const router = useRouter();
+const route = useRoute();
 
 function toggleLeftDrawer() {
   leftDrawerOpen.value = !leftDrawerOpen.value;
+}
+
+async function goPage(page: string) {
+  await router.push(page);
+}
+
+function checkPath(page: string) {
+  if (page == route.fullPath) {
+    return true;
+  } else {
+    return false;
+  }
 }
 </script>
